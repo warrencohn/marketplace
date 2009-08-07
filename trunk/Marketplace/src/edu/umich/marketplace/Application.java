935ef6c92d4fd59acb6c20ed04cdaf6f7db3cadd
gavineadie@gmail.com <gavineadie@gmail.com@fd535186-bfb5-11dd-becf-13071112e4e2>
2009-08-07 23:32:08 +0000

    start to move towards Wonder's "Properties" class

--- trunk/Marketplace/src/edu/umich/marketplace/Application.java
+++ trunk/Marketplace/src/edu/umich/marketplace/Application.java
@@ -15,20 +15,29 @@ import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 
+import com.ramsayconz.wocore.CoreApplication;
 import com.ramsayconz.wocore.CoreAssistance;
+import com.ramsayconz.wocore.CoreHelpRepo;
 import com.webobjects.appserver.WOApplication;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOResponse;
+import com.webobjects.eoaccess.EOModelGroup;
+import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.foundation.NSNotification;
 import com.webobjects.foundation.NSNotificationCenter;
 import com.webobjects.foundation.NSSelector;
+import com.webobjects.foundation._NSUtilities;
 
 import edu.umich.marketplace.eof.ApplicationModel;
 import er.extensions.appserver.ERXApplication;
+import er.extensions.eof.ERXEC;
 
-public class Application extends ITCSApplication {
+public class Application extends com.ramsayconz.wocore.CoreApplication {
 	private static final Logger 		logger = Logger.getLogger(Application.class);
 
+	private EOEditingContext    	    _applicationEC;
+	private ColorPalette				_colorPalette;
+	public boolean                  	_handleExceptions;
 	private ApplicationModel			_applicationModel;
 
 	public ApplicationModel getApplicationModel() {
@@ -72,6 +81,34 @@ public class Application extends ITCSApplication {
 		logger.trace("--> sleep()");
     }
 
+	// -----------------------------------------------------------------------------------------------
+
+	public EOEditingContext getApplicationEC() {
+		return _applicationEC;
+	}
+
+	// -----------------------------------------------------------------------------------------------
+
+	public ColorPalette getColors() {
+		return _colorPalette;
+	}
+
+	public String getBannerBgColor() {						// used in binding
+		return _colorPalette.getBannerBackgroundColor();
+	}
+
+	public String getDarkAccentColor() {
+		return _colorPalette.getDarkAccentColor();
+	}
+
+	public String getLightAccentColor() {
+		return _colorPalette.getLightAccentColor();
+	}
+
+	public String getInstructionsColor() {
+		return _colorPalette.getInstructionsColor();
+	}
+
 //----------------------------------------------------------------------------------------------------------------
 //  N   N      OOO      TTTTTT     III     FFFF     III      CCC      AAA      TTTTTT     III      OOO      N   N
 //  NN  N     O   O       TT        I      F         I      C        A   A       TT        I      O   O     NN  N
@@ -92,8 +129,33 @@ public class Application extends ITCSApplication {
 	 */
 	@Override
 	public void applicationWillFinishLaunching() {
-		logger.trace("--> applicationWillFinishLaunching");
 		super.applicationWillFinishLaunching();
+
+		logger.trace("--> applicationWillFinishLaunching");
+
+		CoreApplication.properties.alphaDump(true);
+		setCachingEnabled(CoreApplication.properties.getBoolean("productionDeploy", "FALSE"));
+		setSessionTimeOut(new Integer(CoreApplication.properties.getInt("sessionTimeout", "3600")));
+
+		_handleExceptions = CoreApplication.properties.getBoolean("handleExceptions", "TRUE");
+		String helpFile = CoreApplication.properties.getString("applicationHelpFile","");
+
+		_colorPalette = ColorPalette.getColorPalette();
+
+		logger.trace(EOModelGroup.defaultGroup());								// check we got them all ?
+
+		_applicationEC = ERXEC.newEditingContext();
+		_applicationEC.setUndoManager(null);
+
+        _helpRepo = new CoreHelpRepo(this.name());
+        _helpRepo.LoadHelpData(resourceManager().inputStreamForResourceNamed(helpFile, null, null));
+		logger.trace("HelpRepo=" + _helpRepo.prettyPrint());
+
+        logger.warn(" ~             JavaMail: " + 
+        		((_NSUtilities.classWithName("javax.mail.Session") == null) ? "not " : "") + "available.");
+        logger.warn(" ~            SMTP host: " + SMTPHost());
+		logger.warn(" ~ directConnectEnabled: " + isDirectConnectEnabled());
+		logger.warn(" ~       cachingEnabled: " + isCachingEnabled());
 
 		startTenMinuteTimerTask();
 		startDayChangeTimerTask();
